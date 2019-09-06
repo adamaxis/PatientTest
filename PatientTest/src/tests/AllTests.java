8b686f88866fc67cf27db559035b46084d61f96f
Draper, Daniel <ddraper@dmacc.edu>
2019-09-05 22:46:33 -0500

    Update

--- PatientTest/src/tests/AllTests.java
+++ PatientTest/src/tests/AllTests.java
@@ -4,6 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
+// AllTests, which does the same thing as TestRunner but with a suite instead
+
 @RunWith(Suite.class)
 @SuiteClasses({ PatientBMITester.class, PatientTester.class })
 public class AllTests {
--- PatientTest/src/tests/TestRunner.java
+++ PatientTest/src/tests/TestRunner.java
@@ -2,11 +2,19 @@ package tests;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-// test runner, which will run everything
+
+// test runner, which will run all tests within PatientTester + PatientBMITester
+// this isn't necessary, but I left it in just to demonstrate it
 public class TestRunner {
 	public static void main(String[] args) {
-		// execute our tests
+		// execute our PatientTester tests
 		Result result = JUnitCore.runClasses(PatientTester.class);
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		System.out.println(result.wasSuccessful());
+		// now execute PatientBMITester tests
+		result = JUnitCore.runClasses(PatientBMITester.class);
 		for (Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
